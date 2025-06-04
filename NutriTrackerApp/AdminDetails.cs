18e2b06c6c982e4adc8375ca685829aa75c6a3e4
ac147616@avcol.school.nz <ac147616@avcol.school.nz>
2025-06-05 10:33:44 +1200

    Made a new class file called AdminDetails

---
+++ NutriTrackerApp/AdminDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriTrackerApp
+{
+    public class AdminDetails
+    {
+        public int AdminID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailID { get; set; }
+        public string Passwordkey { get; set; }
+        public AdminDetails(int adminID, string firstName, string lastName, string emailID, string passwordkey)
+        {
+            AdminID = adminID;
+            FirstName = firstName;
+            LastName = lastName;
+            EmailID = emailID;
+            Passwordkey = passwordkey;
+        }
+    }
+}
