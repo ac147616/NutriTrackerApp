5aa6d553372139f0a7f6d8752500605a23e29746
ac147616@avcol.school.nz <ac147616@avcol.school.nz>
2025-06-10 09:11:05 +1200

    Moved DisplayOptions() and Run() to Menu class. I will probably change the names of these later.

--- NutriTrackerApp/Menu.cs
+++ NutriTrackerApp/Menu.cs
@@ -18,5 +18,66 @@ namespace NutriTrackerApp
             Options = options;
             SelectedIndex = 1;
         }
+
+        private void DisplayOptions()
+        {
+            Console.WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+                string prefix;
+
+                if (i == SelectedIndex)
+                {
+                    prefix = "*";
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prefix = " ";
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+                Console.WriteLine($"{prefix} << {currentOption} >>");
+            }
+
+            Console.ResetColor();
+        }
+        public int Run()
+        {
+            //We need a loop structure so that everytime the user clicks a key it re-renders the console view
+            ConsoleKey keyPressed;
+            do
+            {
+                Console.Clear();
+                DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                //Update SelectedIndex based on arrow keys.
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;  //Selected index closer to the front of the array
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == Options.Length)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+            }
+            while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+        }
     }
 }
