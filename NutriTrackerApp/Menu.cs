4f2521fbbcd223f4cbc0f6cccd191eb587b49c47
ac147616@avcol.school.nz <ac147616@avcol.school.nz>
2025-06-10 09:02:25 +1200

    Moved Memu method to its own class file.

---
+++ NutriTrackerApp/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriTrackerApp
+{
+    class Menu
+    {
+        private int SelectedIndex;
+        private string[] Options;
+        private string Prompt;
+
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+            SelectedIndex = 1;
+        }
+    }
+}
