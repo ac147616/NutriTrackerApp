521bd3352a1d8deff05b545dc741fb33aa80fde9
ac147616@avcol.school.nz <ac147616@avcol.school.nz>
2025-06-10 08:40:08 +1200

    Created changes in ConsoleView, trying to change the way the options are displayed

--- NutriTrackerApp/ConsoleView.cs
+++ NutriTrackerApp/ConsoleView.cs
@@ -7,7 +7,78 @@ using System;
 
 public class ConsoleView
 {
-	public string LogIn()
+    private int SelectedIndex;
+    private string[] Options;
+    private string Prompt;
+    public void Menu(string prompt, string[] options)
+    {
+        Prompt = prompt;
+        Options = options;
+        SelectedIndex = 1;
+    }
+    private void DisplayOptions()
+    {
+        Console.WriteLine(Prompt);
+        for (int i = 0; i < Options.Length; i++)
+        {
+            string currentOption = Options[i];
+            string prefix;
+
+            if (i == SelectedIndex)
+            {
+                prefix = "*";
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                prefix = " ";
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+            Console.WriteLine($"{prefix} << {currentOption} >>");
+        }
+
+        Console.ResetColor();
+    }
+
+    public int Run()
+    {
+        //We need a loop structure so that everytime the user clicks a key it re-renders the console view
+        ConsoleKey keyPressed;
+        do
+        {
+            Console.Clear();
+            DisplayOptions();
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            keyPressed = keyInfo.Key;
+
+            //Update SelectedIndex based on arrow keys.
+            if (keyPressed == ConsoleKey.UpArrow)
+            {
+                SelectedIndex--;  //Selected index closer to the front of the array
+                if (SelectedIndex == -1)
+                {
+                    SelectedIndex = Options.Length - 1;
+                }
+            }
+            else if (keyPressed == ConsoleKey.DownArrow)
+            {
+                SelectedIndex++;
+                if (SelectedIndex == Options.Length)
+                {
+                    SelectedIndex = 0;
+                }
+            }
+        }
+        while (keyPressed != ConsoleKey.Enter);
+
+        return SelectedIndex;
+    }
+
+    public string LogIn()
 	{
 		Console.WriteLine("Hey! How would you like to use this interface?");
 		Console.WriteLine("\n[1] New user");
