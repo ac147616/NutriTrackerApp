a0fcea62a5efc30e0a1d490d95d2618030649c15
ac147616@avcol.school.nz <ac147616@avcol.school.nz>
2025-07-03 21:07:40 +1200

    created a new class called Goals

---
+++ NutriTrackerApp/Goals.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriTrackerApp
+{
+    class Goals
+    {
+        public int GoalID { get; set; }
+        public int UserID { get; set; }
+        public int DietPlanID { get; set; }
+        public string Goal { get; set; }
+        public string DateStarted { get; set; }
+        public string DateEnded { get; set; }
+
+        public Goals(int goalID, int userID, int dietPlanID, string goal, string dateStarted, string dateEnded)
+        {
+            GoalID = goalID;
+            UserID = userID;
+            DietPlanID = dietPlanID;
+            Goal = goal;
+            DateStarted = dateStarted;
+            DateEnded = dateEnded;
+        }
+    }
+}
