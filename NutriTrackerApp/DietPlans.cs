af745f462bedae49f98e2ecc778bcfde8161ee81
ac147616@avcol.school.nz <ac147616@avcol.school.nz>
2025-07-03 21:01:16 +1200

    created a diet plans class

---
+++ NutriTrackerApp/DietPlans.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriTrackerApp
+{
+    class DietPlans
+    {
+        public int DietPlanID { get; set; }
+        public string DietPlan { get; set; }
+        public int CaloriesTarget { get; set; }
+        public int ProteinsTarget { get; set; }
+        public int CarbohydratesTarget { get; set; }
+        public int FatsTarget { get; set; }
+
+        public DietPlans(int dietPlanID, string dietPlan, int caloriesTarget, int proteinsTarget, int carbohydratesTarget, int fatsTarget)
+        {
+            DietPlanID = dietPlanID;
+            DietPlan = dietPlan;
+            CaloriesTarget = caloriesTarget;
+            ProteinsTarget = proteinsTarget;
+            CarbohydratesTarget = carbohydratesTarget;
+            FatsTarget = fatsTarget;
+        }
+    }
+}
