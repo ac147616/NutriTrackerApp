b4a17db4265195c918506ea0d8e7d9e237c8eca4
ac147616@avcol.school.nz <ac147616@avcol.school.nz>
2025-07-03 21:09:29 +1200

    created a new class DailyLog

---
+++ NutriTrackerApp/DailyLog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriTrackerApp
+{
+    class DailyLog
+    {
+        public int LogID { get; set; }
+        public int UserID { get; set; }
+        public int FoodID { get; set; }
+        public string MealTime { get; set; }
+        public string DateLogged { get; set; }
+
+        public DailyLog(int logID, int userID, int foodID, string mealTime, string dateLogged)
+        {
+            LogID = logID;
+            UserID = userID;
+            FoodID = foodID;
+            MealTime = mealTime;
+            DateLogged = dateLogged;
+        }
+    }
+}
